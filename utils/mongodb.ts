fefeb8c27c04577a2b6dd6af7c79eb0bae0b3205
Viktor Hurtig <Viktor.hurtig@outlook.com>
2023-07-02 16:11:12 +0200

    fix(OverwriteModelError): fixed error from overwriting my Todo Model.

--- utils/mongodb.ts
+++ utils/mongodb.ts
@@ -1,23 +1,23 @@
-import mongoose, { Model } from "mongoose"
+import mongoose, { Model, Schema } from "mongoose"
+
+// Define the TodoSchema
+const TodoSchema = new mongoose.Schema({
+  item: String,
+  completed: Boolean,
+})
+
+// Define the Todo model based on the TodoSchema
+const Todo = mongoose.models.Todo || mongoose.model("Todo", TodoSchema)
 
 // CONNECTING TO MONGOOSE (Get Database Url from .env.local)
 const { MONGODB_URL } = process.env
 
-// connection function
+// Connection function
 export const connect = async () => {
   const conn = await mongoose
     .connect(MONGODB_URL as string)
     .catch(err => console.log(err))
   console.log("Mongoose Connection Established")
-
-  // OUR TODO SCHEMA
-  const TodoSchema = new mongoose.Schema({
-    item: String,
-    completed: Boolean,
-  })
-
-  // OUR TODO MODEL
-  const Todo = mongoose.models.todo || mongoose.model("Todo", TodoSchema)
 
   return { conn, Todo }
 }
